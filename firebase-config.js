0b6e8a54afdab7f67a9305fb5dcefaab823afc48
wuttichaikoji-oss <wuttichaikoji@gmail.com>
2026-03-21 11:49:00 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -7,11 +7,12 @@ window.FIREBASE_CONFIG = {
   appId: "1:39856673085:web:29473fd50949d0a4297554",
   measurementId: "G-N0X69132CQ"
 };
-window.FIREBASE_TASKS_COLLECTION = "hk_tasks_v19";
-window.FIREBASE_LOGS_COLLECTION = "hk_logs_v19";
+window.FIREBASE_TASKS_COLLECTION = "multi_dept_tasks_v1";
+window.FIREBASE_LOGS_COLLECTION = "multi_dept_logs_v1";
+window.FIREBASE_USERS_COLLECTION = "multi_dept_users_v1";
 window.FIREBASE_DEVICE_TOKENS_COLLECTION = "device_tokens";
 window.FIREBASE_VAPID_KEY = "BG9P817jX94OZgG3vqV_Dq8D8MwndzrTvwe-ByzZf5I_B--Wffr15N3ga08Fzxs_724Y4VvY-MQ81ZtgeHTRiXM";
-window.FIREBASE_ENABLED = (
+window.FIREBASE_ENABLED = !!(
   window.FIREBASE_CONFIG &&
   window.FIREBASE_CONFIG.projectId
 );
